52c19ec7081449d910a3489b22e2e39783deed3c
bobhoman <bobhoman25@gmail.com>
2017-12-20 17:19:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var recipes = { eggs: 3, flour: "2 cups", milk: "1 cup" };
 
-// function updateObjectWithKeyAndValue(object, key, value) {
-//   object[key] = value
-//
-//   return object
-// }
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
 //
 // function updateObjectWithKeyAndValue(object, key, value) {
 //   Object.assign( {}, object, {[key]: value} )
