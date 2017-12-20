978634e4341271551983349e668c5922e9596a55
bobhoman <bobhoman25@gmail.com>
2017-12-20 17:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValueTwo(object, key, value) {
   Object.assign( {}, object, {[key]: value} )
 
   return object
